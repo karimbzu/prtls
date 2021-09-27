7344c36daf645e8b661fe9a57406c804c9640101
milancr <milan.ciudadreal@gmail.com>
2021-09-26 22:19:41 -0400

    adjust styling convert to yarn

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -1,16 +1,34 @@
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Logo from '../../public/images/main-logo.png';
+import MobileLogo from '../../public/images/mobile-logo.png';
+import { isMobile } from 'react-device-detect';
+
 import styles from './navbar.module.scss';
 export default function Navbar() {
+	console.log(isMobile);
+	const [mobile, setMobile] = useState<Boolean | undefined>(undefined);
+	useEffect(() => {
+		setMobile(isMobile);
+	}, []);
 	return (
 		<div className={styles.navContainer}>
 			<div className={styles.logoContainer}>
-				<Image
-					src={Logo}
-					alt="Portals.art"
-					layout="intrinsic"
-					priority={true}
-				/>
+				{mobile ? (
+					<Image
+						src={MobileLogo}
+						alt="Portals.art"
+						layout="intrinsic"
+						priority={true}
+					/>
+				) : (
+					<Image
+						src={Logo}
+						alt="Portals.art"
+						layout="intrinsic"
+						priority={true}
+					/>
+				)}
 			</div>
 		</div>
 	);
